f783076dbcf906793415fb53422586ce6ec22979
ayshadairi <ayshadairi@gmail.com>
2023-12-31 19:05:03 -0330

    Add project files.

---
+++ CodeFistExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace CodeFistExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new SchoolContext())
+            {
+                context.Database.CreateIfNotExists();
+
+                var student = new Student
+                {
+                    Name = "Sally Smith",
+                    EnrollmentDate = DateTime.Now
+                };
+
+                context.Students.Add(student);
+                context.SaveChanges();
+
+                Console.WriteLine("Student added: " + student.Name);
+            }
+
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CodeFistExample/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data.Entity;
+
+
+namespace CodeFistExample
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
